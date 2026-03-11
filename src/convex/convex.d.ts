aca243848851dc541cfc6ac23241275a372046ff
Dev <W71245208@gmail.com>
2026-03-11 21:21:19 +0530

    Remove convex from project

--- src/convex/convex.d.ts
+++
@@ -1,9 +0,0 @@
-import { Doc } from "./_generated/dataModel";
-
-declare global {
-  interface Window {
-    convex: typeof import("convex");
-  }
-}
-
-export {};
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ConvexProviderWithAuth } from './convex/client'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConvexProviderWithAuth>
-      <App />
-    </ConvexProviderWithAuth>
+    <App />
   </StrictMode>,
 )
